6c0b963ebbf856a652d394b4ba2a8088f0743fa6
Bank5323 <Phipat_Singmeesri@kkumail.com>
2022-06-09 01:36:40 +0700

    Create done button and add method Makedone.

--- TodoListWeb/Controllers/TodoesController.cs
+++ TodoListWeb/Controllers/TodoesController.cs
@@ -122,6 +122,49 @@ namespace TodoListWeb.Controllers
             return View(todoes);
         }
 
+        // GET: Todoes/Makedone/?id
+        public async Task<IActionResult> Makedone(int? id)
+        {
+            // quarry data from id
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var todoes = await _context.Todoes.FindAsync(id);
+            if (todoes == null)
+            {
+                return NotFound();
+            }
+
+            // Update value
+            todoes.Check = true;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // To change Check value
+                    todoes.Check = true;
+
+                    _context.Update(todoes);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TodoesExists(todoes.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return NotFound();
+        }
+
+
         // GET: Todoes/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
